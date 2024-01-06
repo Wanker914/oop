d943b91a470f7473a9954d0c922026bcd6d25cf1
Wanker914 <147255055+Wanker914@users.noreply.github.com>
2024-01-06 23:07:18 +0300

    Delete lr02_cpp directory

--- lr02_cpp/Man.cpp
+++
@@ -1,3 +0,0 @@
-#include "Man.h"
-Man::Man(int lName){pName=new char[lName+1];}
-Man::~Man(){delete []pName;}
--- lr02_cpp/Man.h
+++
@@ -1,9 +0,0 @@
-class Man {
-    public:
-    Man(int lName=30);   //Конструктор 
-    ~Man();             //Деструктор    
-    private:
-    char* pName;
-    int birth_year;
-    float pay;
-};
